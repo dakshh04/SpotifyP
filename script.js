9b0b1d9a8c0d817bb70134206bb18d3104e976a4
Dakshh <dakshhsingh4902@gmail.com>
2023-05-18 12:12:24 +0530

    js added

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
+console.log("Welcome ");
+
+let songIndex = 0;
 let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar')
+
+let songs = [
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
+]
+
+
 // audioElement.play();
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+    }
+})
+
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate')
+})
