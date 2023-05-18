0a633b4af48b1d86121ad0d977a6f9e0319c0075
Dakshh <dakshhsingh4902@gmail.com>
2023-05-18 14:01:36 +0530

    files modified - functions added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar')
+let gif = document.getElementById('gif')
+
 
 let songs = [
     {songName: "Heroes Tonight ", filePath: "1.mp3", coverPath: "covers/1.jpg"},
@@ -22,15 +24,22 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
-        
+        gif.style.opacity = 0;
     }
 })
 
-myProgressBar.addEventListener('timeupdate', ()=>{
-    console.log('timeupdate')
+audioElement.addEventListener('timeupdate', ()=>{
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
 })
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
+})
+ 
